47d4b29309bfaed440547234b96ea364863fafad
Manish <mannyinwork@gmail.com>
2024-07-25 12:59:16 +0530

    sdf

--- ModernWebDev/script.js
+++ ModernWebDev/script.js
@@ -3,10 +3,64 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-function circleMouseFollower() {
-    window.addEventListener("mousemove", function(dets) {
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+function firstPageAnim(){
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1,
+        ease: Expo.easeInOut
+    })
+
+    .to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 1.5,
+        delay:-1,
+        stagger: .2
+    })
+
+    .from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
     })
 }
 
+var timeout;
+
+function circleChaptaKaro(){
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
+    window.addEventListener("mousemove", function(dets){
+        clearTimeout(timeout);
+        xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(.8, 1.2, dets.clientY - yprev);
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+        circleMouseFollower(xscale, yscale);
+
+        timeout = setTimeout(function() {
+            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+        }, 100)
+    })
+}
+
+function circleMouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function(dets) {
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+    })
+}
+
+circleChaptaKaro();
 circleMouseFollower();
+firstPageAnim();
